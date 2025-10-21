c15a0026af164deafc9ce00655833504056ef5d0
Scott M Anderson <anderson.scott.m@icloud.com>
2025-10-21 10:42:53 -0600

    fix compiler warning [-Wint-to-pointer-cast]
    warning: cast to 'const char *' from smaller integer type 'int' [-Wint-to-pointer-cast]
     3987 |                 m_findStr.Append ((const char *)event.GetKeyCode());
          |                                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/treelistctrl.cpp
+++ src/treelistctrl.cpp
@@ -3984,7 +3984,7 @@ void wxTreeListMainWindow::OnChar (wxKeyEvent &event) {
         default:
             if (event.GetKeyCode() >= (int)' ') {
                 if (!m_findTimer->IsRunning()) m_findStr.Clear();
-                m_findStr.Append ((const char *)event.GetKeyCode());
+                m_findStr.Append (static_cast<char>(event.GetKeyCode()));
                 m_findTimer->Start (FIND_TIMER_TICKS, wxTIMER_ONE_SHOT);
                 wxTreeItemId prev = m_curItem? (wxTreeItemId*)m_curItem: (wxTreeItemId*)NULL;
                 while (true) {
